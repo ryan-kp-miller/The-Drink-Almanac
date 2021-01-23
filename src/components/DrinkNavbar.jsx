2bfc80f315bbd94b7634c0c6f00e37c401ce160f
Ryan Miller <Ryan.kp.Miller@gmail.com>
2021-01-22 20:30:48 -0600

    Adjust padding for navbar

--- src/components/DrinkNavbar.jsx
+++ src/components/DrinkNavbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 import { Navbar, Nav} from 'react-bootstrap'
 
 const DrinkNavbar = () => {
-    const padding = {paddingRight: 35}
+    const padding = {paddingLeft: 35}
     return (
         <Navbar style={{width:"100%"}} className="justify-content-center" bg="dark" variant="dark">
             <Navbar.Brand as={Link} style={padding} to="/">The Drink Almanac</Navbar.Brand>
