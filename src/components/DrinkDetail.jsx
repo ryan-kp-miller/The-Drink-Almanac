b0ff455e8228492db51269e27e3a0564f13cb09f
Ryan Miller <Ryan.kp.Miller@gmail.com>
2021-01-24 17:35:19 -0600

    Create separate component to handle the detail view of all drinks, random or otherwise

---
+++ src/components/DrinkDetail.jsx
@@ -0,0 +1,42 @@
+import { Container, Row, Col, Image } from 'react-bootstrap'
+
+const DrinkDetail = ({ drinkJSON, getDrinkHandler }) => {
+    return (
+        <div id="drink-detail-background">
+            <Container>
+                <div className="drink-jumbo">
+                    <h2 className="text-center">{ drinkJSON.drinkName }</h2>
+                    {
+                        getDrinkHandler ?
+                        <div className="d-flex justify-content-center">
+                            <button type="button" className="btn btn-primary" onClick={ getDrinkHandler } variant="primary">Find a drink!</button>
+                        </div> :
+                        undefined
+                    }
+                </div>
+
+                <Container>
+                    <div className="row" id="drink-details">
+                        <Col lg={7} xs={12}>
+                            <Row>
+                                <ul>
+                                    { 
+                                        drinkJSON.ingredients.map((inst) => <li key={`${inst}`}>{`${inst}`}</li>)
+                                    }
+                                </ul> 
+                            </Row>
+                            <Row>
+                                <p> {drinkJSON.instructions} </p>
+                            </Row>
+                        </Col>
+                        <div className="col-lg-5 col-sm-3 d-flex justify-content-center">
+                            {<Image src={ drinkJSON.imgURL } alt="..." id="drink-detail-img" className="img-thumbnail" />}
+                        </div>
+                    </div>
+                </Container>
+            </Container>
+        </div>
+    )
+}
+
+export default DrinkDetail
--- src/components/RandomDrink.jsx
+++ src/components/RandomDrink.jsx
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react'
-import { Container, Row, Col, Image } from 'react-bootstrap'
-import getRandomDrink from '../services/getRandomDrink' 
+import getRandomDrink from '../services/getDrinkData' 
+import DrinkDetail from './DrinkDetail'
 
-const RandomDrink = ({ isDesktop}) => {
+const RandomDrink = () => {
     const [drinkJSON, setDrinkJSON] = useState({
         drinkID: "",
         drinkName: "",
@@ -17,36 +17,7 @@ const RandomDrink = ({ isDesktop}) => {
     useEffect(getDrinkHandler, [])
 
     return (
-        <div id="random-drink-background">
-            <Container>
-                <div className="drink-jumbo">
-                    <h2 className="text-center">{ drinkJSON.drinkName }</h2>
-                    <div className="d-flex justify-content-center">
-                        <button type="button" className="btn btn-primary" onClick={ getDrinkHandler } variant="primary">Find a drink!</button>
-                    </div>
-                </div>
-
-                <Container>
-                    <div className="row" id="random-drink-details">
-                        <Col lg={7} xs={12}>
-                            <Row>
-                                <ul>
-                                    { 
-                                        drinkJSON.ingredients.map((inst) => <li key={`${inst}`}>{`${inst}`}</li>)
-                                    }
-                                </ul> 
-                            </Row>
-                            <Row>
-                                <p> {drinkJSON.instructions} </p>
-                            </Row>
-                        </Col>
-                        <div className="col-lg-5 col-sm-3 d-flex justify-content-center">
-                            {<Image src={ drinkJSON.imgURL } alt="..." id="random-drink-img" className="img-thumbnail" />}
-                        </div>
-                    </div>
-                </Container>
-            </Container>
-        </div>
+        <DrinkDetail drinkJSON={drinkJSON} getDrinkHandler={getDrinkHandler}/>
     )
 }
 
