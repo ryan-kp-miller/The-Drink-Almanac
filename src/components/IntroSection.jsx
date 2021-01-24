0d6849b6f6524cfbcf89d84894dcc56c1838d683
Ryan Miller <Ryan.kp.Miller@gmail.com>
2021-01-24 13:09:41 -0600

    Modify intro section description

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -4,7 +4,7 @@ const IntroSection = () => {
     return (
     <div className="intro-section">
         <h2 className="brand-font text-white text-center">The Drink Almanac</h2>
-        <p className="lead text-white text-center">Learn how to make any alcoholic drink.</p>
+        <p className="lead text-white text-center">Learn how to make any drink.</p>
     </div>
     )
 }
