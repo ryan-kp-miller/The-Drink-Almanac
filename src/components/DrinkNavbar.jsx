2440c6196def6d69e689f9ed33e4205568f5febe
Ryan Miller <Ryan.kp.Miller@gmail.com>
2021-01-24 13:47:46 -0600

    Adjust navbar to collapse and uncollapse when a link is clicked

--- src/components/DrinkNavbar.jsx
+++ src/components/DrinkNavbar.jsx
@@ -1,16 +1,27 @@
+import React, { useState } from 'react'
 import { Link } from "react-router-dom"
 import { Navbar, Nav} from 'react-bootstrap'
 
 const DrinkNavbar = () => {
+    const [expanded, setExpanded] = useState(false)
+    const setNavbarExpanded = () => setExpanded(!expanded)
+    const unexpandNavbar = () => {
+        console.log("unexpanded")
+        if (expanded) {setExpanded(false)}
+    }
     const padding = {paddingLeft: 35}
     return (
-        <Navbar style={{width:"100%"}} className="justify-content-center" bg="dark" variant="dark">
-            <Navbar.Brand as={Link} style={padding} to="/">The Drink Almanac</Navbar.Brand>
-            <Nav className="mr-auto">
-            <Nav.Link as={Link} style={padding} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} style={padding} to="/random-drink">Random Drink</Nav.Link>
-            <Nav.Link as={Link} style={padding} to="/search-drink">Search for a Drink</Nav.Link>
-            </Nav>
+        <Navbar style={{width:"100%"}} className="justify-content-center" bg="dark" expand="lg" variant="dark"
+                onToggle={setNavbarExpanded} expanded={expanded}>
+            <Navbar.Brand className="mr-auto" as={Link} style={padding} to="/">The Drink Almanac</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={setNavbarExpanded} />
+            <Navbar.Collapse className="ml-auto" id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Link as={Link} className="ml-auto" style={padding} onClick={unexpandNavbar} to="/">Home</Nav.Link>
+                    <Nav.Link as={Link} className="ml-auto" style={padding} onClick={unexpandNavbar} to="/random-drink">Random Drink</Nav.Link>
+                    <Nav.Link as={Link} className="ml-auto" style={padding} onClick={unexpandNavbar} to="/search-drink">Search for a Drink</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     )
 }
