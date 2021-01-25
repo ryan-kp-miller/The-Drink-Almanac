1f4839f22d33d02c3ab2253d8c0782333225edf1
Ryan Miller <ryan.kp.miller@gmail.com>
2021-01-25 11:22:16 -0600

    Adjust search form to let user know when no drinks were found

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Container, Col } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import { formatDrinkJSON, getDrinksByName } from '../services/getDrinkData'
 import DrinkList from './DrinkList'
 import PageHeader from './PageHeader'
@@ -7,15 +7,20 @@ import PageHeader from './PageHeader'
 
 const SearchForm = () => {
     const [searchText, setSearchText] = useState("")
+    const [hasSearched, setHasSearched] = useState(false)
     const [drinkJSONArray, setDrinkArray] = useState([])
     const searchForDrinks = (drinkName) => {
+        setHasSearched(true)
         const responseArray = getDrinksByName(drinkName)
         responseArray.then(drinks => {
+            if (!drinks) {
+                setDrinkArray([])
+                return
+            }
             setDrinkArray(
                 drinks.map(drink => formatDrinkJSON(drink))
             )
         })
-        console.log(drinkJSONArray)
     }
 
     return (
@@ -23,12 +28,18 @@ const SearchForm = () => {
             <PageHeader pageTitle="Search for a Drink by Name" additionalDiv={
                 <Container className="input-group mt-3 justify-content-center">
                     <input className="input-group-text" type="text" onChange={event => setSearchText(event.target.value)} placeholder="Enter the name of a drink"/> 
-                    <div class="input-group-append">
+                    <div className="input-group-append">
                         <button className="btn btn-primary" onClick={() => searchForDrinks(searchText)}>Search</button>
                     </div>
                 </Container>
             }/>
-            <DrinkList drinkJSONArray={drinkJSONArray} />             
+            {
+                hasSearched ?
+                    drinkJSONArray.length > 0 ?
+                        <DrinkList drinkJSONArray={drinkJSONArray} /> :
+                        <p className="text-center">Sorry, no drinks were found with that name.</p> :
+                    undefined
+            }
         </div>
     )
 }
