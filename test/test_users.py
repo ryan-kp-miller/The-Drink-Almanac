ba8e0104e1d6ae03f8c2bf2a8fa0a7f4f220e2c3
Ryan Miller <ryan.kp.miller@gmail.com>
2021-06-24 17:16:59 -0500

    Adjust favorites endpoint to require JWT

--- test/test_users.py
+++ test/test_users.py
@@ -3,12 +3,13 @@ import requests
 API_BASE_URL = "http://127.0.0.1:5000/api/"
 
 def print_response(response):
-    data = response.json()
     print(f"Response Status Code: {response.status_code}")
-    if 'message' in data:
-        print(f"Server message: {data['message']}")
-    else:
-        print("Server data keys: ", data.keys())
+    if response.status_code != 500:
+        data = response.json()
+        if 'message' in data:
+            print(f"Server message: {data['message']}")
+        else:
+            print("Server data keys: ", data.keys())
 
 # check to see if the user already exists    
 user_payload = {
@@ -29,25 +30,24 @@ else:
 # log in as the user
 response = requests.post(API_BASE_URL + "login", json=user_payload)
 print_response(response)
+access_token = response.json()['access_token']
 
 # add a favorite to the test user
 payload_list = [
-    {
-        'user_id': user_id,
-        'drink_id': 11007,
-    },
-    {
-        'user_id': user_id,
-        'drink_id': 11001,
-    }
+    {'drink_id': 11007},
+    {'drink_id': 11001},
 ]
 
 for payload in payload_list:
-    response = requests.post(API_BASE_URL + "favorite", json=payload)
+    response = requests.post(
+        API_BASE_URL + "favorite", 
+        json=payload,
+        headers={'Authorization': f'Bearer {access_token}'}
+    )
     print_response(response)
 
 
 # delete the test user
-response = requests.delete(API_BASE_URL + "user", json=user_payload)
-print_response(response)
+# response = requests.delete(API_BASE_URL + "user", json=user_payload)
+# print_response(response)
 
