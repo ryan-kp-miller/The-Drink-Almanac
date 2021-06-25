281a5f3dec386e0f0c904a088ad06311fac0c101
Ryan Miller <ryan.kp.miller@gmail.com>
2021-06-25 13:45:39 -0500

    Move PageHeader to DrinkDetail; add favorite/unfavorite button and automatic redirect to login if jwt is expired

--- src/components/DrinkDetail.jsx
+++ src/components/DrinkDetail.jsx
@@ -1,8 +1,101 @@
+import React, {useState, useEffect} from 'react'
 import { Container, Row, Col, Image } from 'react-bootstrap'
+import { addFavorite, deleteFavorite, getUserByJWT } from '../services/userRequests'
+import PageHeader from './PageHeader'
+import Alert from 'react-bootstrap/Alert'
+import { useHistory } from 'react-router-dom'
 
-const DrinkDetail = ({ drinkJSON }) => {
+const DrinkDetail = ({ jwt, drinkJSON }) => {
+    const history = useHistory()
+    const [user, setUser] = useState({'id': undefined, 'favorites': []})
+    const [isFavorited, setIsFavorited] = useState(false)
+    const [favoriteAlert, setFavoriteAlert] = useState()
+    const favoriteEventListener = (event) => {
+        event.preventDefault()
+
+        var alertStyle
+        var alertMessage
+        let promise = undefined
+        if (isFavorited) {
+            promise = deleteFavorite(jwt.access_token, drinkJSON.drinkID)
+        }else {
+            promise = addFavorite(jwt.access_token, drinkJSON.drinkID)
+        }
+        promise.then( () => {
+            alertStyle = "success"
+            alertMessage = `Succesfully ${isFavorited ? "unfavorited" : "favorited"} this drink!`
+            setIsFavorited(!isFavorited)
+            const newUser = user
+            newUser.favorites.push(drinkJSON.drinkID)
+            setUser(newUser)
+        }).catch( error => {
+            console.log(error)
+            // if the token expired, redirect to the login page
+            if(error.response.data.msg === "Token has expired"){
+                history.push("/login")
+            }
+            // otherwise, show error alert
+            else{
+                alertStyle = "danger"
+                alertMessage = `An error occurred when attempting to ${isFavorited ? "unfavorite" : "favorite"} this drink. Please try again later.` 
+            }
+        }).finally(() => {
+            setFavoriteAlert(
+                <Alert className="text-center" variant={alertStyle}>{alertMessage}</Alert>    
+            )
+        })
+    }
+
+    
+    //once the drink data has been retrieved, check if it's been favorited by the user already
+    useEffect(() => {
+        const promise = getUserByJWT(jwt.access_token)
+        promise.then( data => {
+            if(data){
+                setUser(data)
+                if(data.favorites.includes(Number(drinkJSON.drinkID))){
+                    setIsFavorited(true)
+                }else {
+                    setIsFavorited(false)
+                }   
+            } 
+        }).catch( error => {
+            //if token has expired, redirect to the login page
+            console.log(error)
+            if(error.response.status === 401){
+                console.log("error 401")
+                if(error.response.data.msg === "Token has expired"){
+                    history.push("/login")
+                }
+            }
+            setIsFavorited(false)
+        })
+    }, [drinkJSON, jwt])
+
+
     return (
         <div>
+            <PageHeader pageTitle={drinkJSON.drinkName} additionalDiv={
+                <Col className="justify-content-center">
+                    <Container>
+                        <button type="button" className="btn btn-primary row" onClick={ favoriteEventListener } variant="primary">
+                            {
+                                isFavorited ?
+                                    "Unfavorite this drink" :
+                                    "Favorite this drink!" 
+                            }
+                        </button>
+                    </Container>
+                    <Container>
+                        { 
+                            <div className="row container">
+                                <br/>
+                                {favoriteAlert} 
+                            </div>
+                        }
+                    </Container>
+                </Col>
+            }/>
             <Container>
                 <Container>
                     <div className="row" id="drink-details">
