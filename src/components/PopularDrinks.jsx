7ed88e77245d2221b72bac5ea354c42f81b422a4
Ryan Miller <ryan.kp.miller@gmail.com>
2021-06-25 16:36:08 -0500

    Move popular drinks declaration into useEffect to remove warning

--- src/components/PopularDrinks.jsx
+++ src/components/PopularDrinks.jsx
@@ -6,12 +6,15 @@ import { retrieveMultipleDrinks } from '../services/getDrinkData'
 
 const PopularDrinks = () => {   
     const [drinkJSONArray, setDrinkArray] = useState([])
-    const popularDrinkIDs = ["11000", "11001", "11002", 
-    "11003", "11004", "11005", "11006", "11007",
-    "11008", "11009"]
 
     //when page is loaded, make API request for random drink data
-    useEffect( () => retrieveMultipleDrinks(popularDrinkIDs, setDrinkArray), [])
+    useEffect( () => {
+        const popularDrinkIDs = ["11000", "11001", "11002", 
+        "11003", "11004", "11005", "11006", "11007",
+        "11008", "11009"]
+    
+        retrieveMultipleDrinks(popularDrinkIDs, setDrinkArray)
+    }, [])
 
     return (
         <div className="page-background">
