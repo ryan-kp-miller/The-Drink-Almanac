11be37f1dad90603f3c6ffa9e933fe30f16831e1
Ryan Miller <Ryan.kp.Miller@gmail.com>
2021-06-26 21:24:46 -0500

    Update app.run to retrieve port from environment

--- manage.py
+++ manage.py
@@ -26,7 +26,6 @@ def index():
 
 if __name__ == "__main__":
     app.run(
-        host='0.0.0.0', 
         debug=False, 
-        port=os.environ.get('PORT', 80)
+        port=os.environ.get('PORT', 5000)
     )
